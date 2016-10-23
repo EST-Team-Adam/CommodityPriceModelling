f893cab593b69a75243c84674957e5e693d2e95b
mk <mkao006@gmail.com>
2016-10-23 14:44:29 +0800

    Added temporary dataset for commodity price
    As we can not quickly identify a way to convert the .xlsb format to
    a readily usable format, we simply copied the data from the .xlsb
    and saved it as 'igc_goi.csv'. Then the R script performs some basic
    manipulation before saving it in the .feather format.
    
    We will later automate the whole data pipeline.

---
+++ data/commodity_price/process_igc_goi.R
@@ -0,0 +1,23 @@
+library(yaml)
+library(magrittr)
+library(dplyr)
+library(feather)
+
+## Read metadata
+metadata = yaml.load_file("../metadata.yml")
+metadata$start_date = as.Date(metadata$start_date, format = "%Y-%m-%d")
+metadata$end_date = as.Date(metadata$end_date, format = "%Y-%m-%d")
+
+## Read the processed IGC data
+##
+## NOTE(Michael): Ideally we should automise the process.
+igc.df = read.csv(file = "igc_goi.csv", stringsAsFactor = FALSE)
+
+## Remove the redudant column
+processed_igc.df =
+    igc.df %>%
+    select(., select = -X) %>%
+    mutate(., date = as.Date(date, format = "%m/%d/%Y"))
+
+## Save the processed data back as feather format
+write_feather(processed_igc.df, path = "processed_igc.feather")
